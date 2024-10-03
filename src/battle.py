5b2f287c47d63908a5a7e9f5935ec82cebd2cc9e
Zaza <Zazuka2000badzagua@gmail.com>
2024-10-03 13:55:36 +0200

    add buffs for class Warrior

--- src/battle.py
+++ src/battle.py
@@ -5,6 +5,22 @@ from enemy import Goblin # import calss enemy
 def battle(player,goblin):
     print("Battle starts! destroy him")
 
+
+    # check buffs
+
+    if isinstance(player,Warrior):
+        action = input("Choose an action (roar, eat, sleep):").lower()
+
+        if action == "roar":
+            print(player.warrior_roar())
+        elif action == "eat":
+            print(player.warrior_eat())
+        elif action == "sleep":
+            print(player.warrior_sleep())
+        else:
+            print("Invalid buff, attack default")
+        
+
     while player.is_alive_character() and goblin.is_goblin_alive():
         # player's turn
         player.attack(goblin)
