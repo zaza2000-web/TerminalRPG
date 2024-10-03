b0fb58262b49c85178b86ef3de30af6f57345c52
Zaza <Zazuka2000badzagua@gmail.com>
2024-10-03 13:38:05 +0200

    realise to battle basic battle logic

--- src/battle.py
+++ src/battle.py
@@ -1,2 +1,25 @@
-from character import Character # import class character
-from enemy import enemy # import calss enemy
+from character import Character,Warrior # import class character
+from enemy import Goblin # import calss enemy
+
+
+def battle(player,goblin):
+    print("Battle starts! destroy him")
+
+    while player.is_alive_character() and goblin.is_goblin_alive():
+        # player's turn
+        player.attack(goblin)
+        if not goblin.is_goblin_alive():
+            print(f"Goblin {goblin.name} has been defeated")
+            break
+
+        # enemy's turn
+        goblin.attack(player)
+        if not player.is_alive_character():
+            print(f"Player {player.name} has been defeated by {goblin.name}")
+            break
+
+if __name__ == "__main__":
+    player = Warrior("Garen")
+    goblin = Goblin()
+
+    battle(player,goblin)
