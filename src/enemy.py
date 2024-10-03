54a9756175f0048dc845af6d579afb2fb2510979
Zaza <Zazuka2000badzagua@gmail.com>
2024-10-03 13:10:52 +0200

    add attack logic and also add name to goblin class

--- src/enemy.py
+++ src/enemy.py
@@ -1,12 +1,21 @@
 class Goblin:
-    def __init__(self,health=700,mana=35,max_mana=40,attack_power=75):
+    def __init__(self,name="Hermet",health=700,mana=35,max_mana=40,attack_power=75):
         self.health = health
         self.mana = mana
         self.max_mana = max_mana
         self.attack_power = attack_power
+        self.name = name
     
     # check if goblin is alive
 
     def is_goblin_alive(self):
         return self.health > 0
 
+    # add attack logic
+
+    def attack(self,enemy):
+        if self.is_goblin_alive():
+            print(f"{self.name} attack {enemy.name} he deal {self.attack_power}")
+            enemy.health -= self.attack_power
+        else:
+            print(f"{self.name} defeated by {enemy.name}")
