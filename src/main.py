80ea126330c59d1f9022864d61781eb54e30e14a
Zaza <Zazuka2000badzagua@gmail.com>
2024-10-03 17:52:38 +0200

    create while cycle for game

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 from character import Warrior
 from enemy import Goblin
 from battle import battle
+import sys
 
 
 def main():
@@ -9,6 +10,25 @@ def main():
     goblin = Goblin()
     answer = input("Start game? Yes/No: ")
     running = is_game_start(answer)
+    player_statistic = get_player_statistics(player)
+    print(player_statistic)
+
+    while running:
+         try:
+              stats = print("1. check your stats")
+              exit_game = print("2. exit")
+              start_game = print("3. Go battle")
+              user_choice = input("Choice: ")
+              if user_choice == "1":
+                   get_player_statistics(player)
+                   continue
+              elif user_choice == "2":
+                   sys.exit("Goodbye!")
+              elif user_choice == "3":
+                   battle(player,goblin)
+                   break
+         except ValueError:
+              pass
     
  
 
@@ -18,7 +38,11 @@ def is_game_start(answer):
             return True
         else:
             return False
+        
+def get_player_statistics(target):
+    
+    print(f"{target.name} has: ({target.health}/{target.health}HP, Damage: {target.attack_power}. Mana: {target.mana}/{target.mana})")
+    
 
-     
 
 main()
