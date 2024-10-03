a588ecdfb51cfb7188569094d5a22f70fb18029d
Zaza <Zazuka2000badzagua@gmail.com>
2024-10-03 12:52:46 +0200

    start to make enemy class

---
+++ src/enemy.py
@@ -0,0 +1,6 @@
+class Goblin:
+    def __init__(self,health=700,mana=35,max_mana=40,attack_power=75):
+        self.health = health
+        self.mana = mana
+        self.max_mana = max_mana
+        self.attack_power = attack_power
